d195ab4da8211e7dc4c76a41808c584d09aca953
novaladitya <noval.aditya1019@students.unila.ac.id>
2020-10-16 16:56:25 +0700

    Update Parsing Data

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,17 @@
 <?php namespace App\Controllers;
 
-class Home extends BaseController
-{
-	public function index()
-	{
+class Home extends BaseController{
+	public function index(){
 		return view('welcome_message');
 	}
 
 	//--------------------------------------------------------------------
+	public function show(){
+		$data['nama'] = 'Noval Aditya Marlon';
+		$data['jurusan'] = 'Ilmu Komputer';
 
+		echo view('mahasiswa/header');
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer');
+	}
 }
